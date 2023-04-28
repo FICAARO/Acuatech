0e94ccbb86bf6ec4ad82bc4b504bb190e8fd9076
dasjero98772 <jero98772@gmail.com>
2023-04-28 13:26:21 -0500

    fix images

--- TiendaPeces/TiendaPeces/urls.py
+++ TiendaPeces/TiendaPeces/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 #from TiendaPeces.views import *
 #import TiendaPecesWebApp.views 
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path('tienda',include('TiendaApp.urls')),
     #path('index/', TiendaPecesWebApp.views.site.index,name="index")
 ]
+
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
