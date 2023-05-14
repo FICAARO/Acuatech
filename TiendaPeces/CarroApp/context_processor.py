6d5e3b75ddad71270f3a3439980351506835103f
jero98772 <jero98772@protonmail.com>
2023-05-14 08:25:33 -0500

    update add context processor

--- TiendaPeces/CarroApp/context_processor.py
+++ TiendaPeces/CarroApp/context_processor.py
@@ -1,8 +1,9 @@
 def importe_tota_carro(request):
 	total=0
-	"""
 	if request.user.is_authenticated:
 		for key,value in request.session["carro"].items():
+			pass
+	"""
 				total+=float(value["precio"]*value["cantidad"])
 	"""
 	return {"importe_tota_carro":total}
