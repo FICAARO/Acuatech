300c1f85d4104f1c4a1e04bf6bb3001e0a7a947d
dasjero98772 <jero98772@gmail.com>
2024-04-15 13:25:53 -0500

    fix some depencies adn update chat seller

--- TiendaPeces/chatSeller/views.py
+++ TiendaPeces/chatSeller/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from llama_cpp import Llama
+import google.generativeai as genai
 import os
 
 def webTranslate(txt,writeIn,translateTo):
@@ -16,6 +17,46 @@ def webTranslate(txt,writeIn,translateTo):
 	from deep_translator import GoogleTranslator 
 	translatedTxt = GoogleTranslator(source=writeIn, target=translateTo).translate(txt)
 	return translatedTxt
+def mixtralinterface(msg,role):
+	answer=""
+	msg=webTranslate(msg,"es","en")
+
+	
+	llm = Llama(
+	  model_path="../models/mixtral-8x7b-v0.1.Q6_K.gguf", 
+	  n_ctx=2048, 
+	  n_threads=24,        
+	  n_gpu_layers=35         # The number of layers to offload to GPU, if you have GPU acceleration available
+	)
+	output = llm(
+	  "{prompt}", # Prompt
+	  max_tokens=512,  # Generate up to 512 tokens
+	  stop=["</s>"],   # Example stop token - not necessarily correct for this specific model! Please check before using.
+	  echo=True        # Whether to echo the prompt
+	)
+
+
+	llm = Llama(model_path="../models/mixtral-8x7b-v0.1.Q6_K.gguf", chat_format="llama-2")  # Set chat_format according to the model you are using
+	answer=llm.create_chat_completion(
+	    messages = [
+	        {"role": role, "content": "Just answer the question and take in mind you are the best seller of fishes, your task is sell the most fishes that you can, if you dont know somthing create it"},
+	        {
+	            "role": "user",
+	            "content": msg#"Hello i want to buy a oscar fishes"
+	        }
+	    ]
+	)
+	answer=answer["choices"][0]["message"]["content"]
+	answer=webTranslate(answer,"en","es")
+	return answer
+def geminiChat(text,rol):
+	MODEL = 'gemini-pro'
+	print('** GenAI text: %r model & prompt %r\n' % (MODEL, text))
+	genai.configure(api_key="")
+	model = genai.GenerativeModel(MODEL)
+	response = model.generate_content("answer :"+text+" as a "+rol)
+	return response.text
+
 def chatSellerIndex(request):
 	role="""As a seasoned fish expert and passionate aquarist, I invite you to embark on an underwater journey where beauty meets functionality, and where every ripple tells a story of life and vitality. At our aquarium emporium, we offer a vast array of products designed to transform your aquatic dreams into stunning realities.
 
@@ -36,40 +77,8 @@ And let's not forget about the foundation of your aquatic masterpiece – the su
 But our commitment to your aquatic journey doesn't end with the sale. We're here to support you every step of the way, whether it's through informative blog posts on proper maintenance routines or engaging social media content showcasing the latest aquascaping trends. And for those eager to dive deeper into the art of aquascaping, we offer seminars and workshops where you can learn advanced techniques and gain hands-on experience.
 
 So why wait? Dive into the world of aquaria with us and discover the endless possibilities of creating your own aquatic masterpiece. Together, we'll turn your underwater dreams into reality."""
-	print(os.getcwd())
-	answer=""
 	msg=request.POST.get("message")
-	msg=webTranslate(msg,"es","en")
-	print(msg)
-
+	answer=""
 	if request.method == 'POST':
-		print(msg)
-		
-		llm = Llama(
-		  model_path="../models/mixtral-8x7b-v0.1.Q6_K.gguf", 
-		  n_ctx=2048, 
-		  n_threads=24,        
-		  n_gpu_layers=35         # The number of layers to offload to GPU, if you have GPU acceleration available
-		)
-		output = llm(
-		  "{prompt}", # Prompt
-		  max_tokens=512,  # Generate up to 512 tokens
-		  stop=["</s>"],   # Example stop token - not necessarily correct for this specific model! Please check before using.
-		  echo=True        # Whether to echo the prompt
-		)
-
-
-		llm = Llama(model_path="../models/mixtral-8x7b-v0.1.Q6_K.gguf", chat_format="llama-2")  # Set chat_format according to the model you are using
-		answer=llm.create_chat_completion(
-		    messages = [
-		        {"role": role, "content": "Just answer the question and take in mind you are the best seller of fishes, your task is sell the most fishes that you can, if you dont know somthing create it"},
-		        {
-		            "role": "user",
-		            "content": msg#"Hello i want to buy a oscar fishes"
-		        }
-		    ]
-		)
-		answer=answer["choices"][0]["message"]["content"]
-		answer=webTranslate(answer,"en","es")
-		print(answer)
+		answer=geminiChat(msg,role)
 	return render(request,"chat.html",{"chatmsg":answer})
